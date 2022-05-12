f8d058dab6044186047cb3d1561421e6838ca749
Isa <isa0021@mail.ru>
2022-05-12 11:27:14 +0300

    footer

--- src/footer.js
+++ src/footer.js
@@ -1,3 +1,7 @@
+import img1 from "../img/facebook.jpg"
+import img2 from "../img/insta.jpg"
+import img3 from "../img/twitter.jpg"
+import img4 from "../img/youtube.jpg"
 export function footerNews() {
   
   const root = document.getElementById('root')
@@ -6,13 +10,29 @@ export function footerNews() {
   
   const contacts = document.createElement('div')
   contacts.classList.add('contacts')
-
+  contacts.textContent = "+7 (999)-999-99-99"
+  
   const icon = document.createElement('div')
-
-  contacts.textContent = "mor"
   icon.textContent = "О нас"
+  icon.a = "#"
+  
+  const sait = document.createElement('div')
+  sait.classList.add('sait')
+  const facebook = document.createElement('img')
+  facebook.src = img1
 
-  foot_main.append(contacts, icon)
+  const insta = document.createElement('img')
+  insta.src = img2
+
+  const twitter = document.createElement('img')
+  twitter.src = img3
+
+  const youtube = document.createElement ('img')
+  youtube.src = img4
+  youtube.a = "#"
+
+  sait.append(facebook,insta,twitter,youtube)
+  foot_main.append(icon, sait, contacts)
   root.after(foot_main)
 }
 export default footerNews
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import footerNews from "./footer";
 import getCategories from "./getCategories";
 import getNews from "./getNews";
+import headerNews from "./header";
 
 getCategories()
 getNews()
 footerNews()
+headerNews()
