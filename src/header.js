47df86fbc17576c4058f1f5436cd5d8b073c87e0
Shamil <shamil>
2022-05-12 12:52:03 +0300

    header

--- src/header.js
+++ src/header.js
@@ -3,16 +3,41 @@ export function headerNews() {
   const root = document.getElementById("root");
 
   const header = document.createElement("div");
-  // const h1 = document.createElement('img')
-  // h1.src = img
+
   header.classList.add("header");
   header.src = img
 
   const title = document.createElement("h1");
   title.classList.add("time");
   title.textContent = 'NEWS'
-  // time.src = img;
-  //  time.innerHTML = date_time()
+  title.style.marginTop = '0px'
+
+  const userFunc = document.createElement('div');
+  userFunc.style.width = '200px'
+  userFunc.style.display = 'flex';
+  userFunc.style.justifyContent = 'space-Between';
+
+  const allDate = document.createElement('div');
+
+  const date = document.createElement('div');
+  const data = new Date();
+  const year = data.getFullYear();
+  const day = data.getDate();
+  
+  const allTime = document.createElement('div');
+  const time = new Date();
+  const hour = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  
+  date.textContent = `Сегодня ${day} мая ${year} года`;
+  allTime.textContent = `Текущее время: ${hour}:${minutes}:${seconds}`
+
+  
+  allDate.prepend(date, allTime);
+
+
 
   const join = document.createElement("div");
   join.classList.add("join");
@@ -21,25 +46,17 @@ export function headerNews() {
 
   const authorization = document.createElement("div");
   authorization.classList.add("author");
-  authorization.textContent = "Авторизация";
+  authorization.textContent = "Регистрация";
 
-  header.append(title, join, authorization);
+
+
+  header.style.display = 'flex';
+  header.style.justifyContent = 'space-Between'
+
+  userFunc.append(join, authorization)
+  header.append(title, allDate, userFunc);
   root.before(header);
 
-  // function date_time()
-  // {
-  //     const current_datetime = new Date();
-  //     const day = zero_first_format(current_datetime.getDate());
-  //     const month = zero_first_format(current_datetime.getMonth()+1);
-  //     const year = current_datetime.getFullYear();
-  //     const hours = zero_first_format(current_datetime.getHours());
-  //     const minutes = zero_first_format(current_datetime.getMinutes());
-
-  //     return day+"."+month+"."+year+" "+hours+":"+minutes;
-  // };
-
-  /* выводим текущую дату и время на сайт в блок с id "current_date_time_block" */
-  // document.querySelector('.time').innerHTML = date_time();
 }
 
 export default headerNews;
