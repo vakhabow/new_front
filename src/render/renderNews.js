40809be7371788fa7efbe799700e1a458a840e2c
керимов алихан <yakhya2809@gmail.com>
2022-05-12 10:39:39 +0300

    fro

--- src/render/renderNews.js
+++ src/render/renderNews.js
@@ -13,6 +13,7 @@ export const render = () => {
 
     initialState.news.forEach((news) => {
       const div = document.createElement("div");
+      div.classList.add("news_list")
       div.textContent = news.title;
       div.addEventListener("click", () => getNewsById(news._id));
 
